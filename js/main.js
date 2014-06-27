ccc59757ecae3a35026a5c08b0a33aa1aefb016e
Josh Strange <josh@joshstrange.com>
2014-06-27 18:25:23 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ function rebuildDimensions() {
 
 (function ($) {
 	$(function () {
-		$('img').attr('http://dl.dropboxusercontent.com/u/2148004/Screenshots'+location.pathname+'.png');
+		$('img').attr('src', 'http://dl.dropboxusercontent.com/u/2148004/Screenshots'+location.pathname+'.png');
 		on_resize(rebuildDimensions);
 		$('img').load(function() {
 			rebuildDimensions();
